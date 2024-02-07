96757bae6f2ea31479cb44f1167527699b1c3f69
Ueslei Carvalho <ueslei_carvalho-f@estudante.sesisenai.org.br>
2024-02-07 15:25:12 -0300

    Arrumado o erro no conversor de alúminio

--- conversor_aluminio.js
+++ conversor_aluminio.js
@@ -1,8 +1,8 @@
  let resistencia = 0
- let resistividadeCobre = 0.0172 // resistividade do condutor de cobre
- let comprimento = 0
- let secaoTransversal = 1
+ let resistividadeAlumi = 0.0282 // resistividade do condutor de alúminio
+ let comprimento = 1555
+ let secaoTransversal = 2.5
 
- resistencia = (resistividadeCobre * comprimento) / secaoTransversal
+ resistencia = (resistividadeAlumi * comprimento) / secaoTransversal
 
  console.log("O valor da resistência é : " + resistencia);
